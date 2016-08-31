30d5cb5917caeb3850170c0da39eaa499ce5dd7e
Sriram <sriram5052@gmail.com>
2016-08-31 21:53:27 +0530

    commited program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace jobspire
+{
+    class Program
+    {
+        private static readonly Random rdm = new Random();    //create a object for random class    
+        static void Main(string[] args)
+        {
+
+           Console.WriteLine(getrandom(Console.ReadLine().ToString())); /*get the input from  
+                                                                          console and pass it to 
+                                                                          the method getrandom and 
+                                                                          print's the result in console */
+            Console.ReadKey(); // to wait for a key from user
+        }
+
+        public static string  getrandom(string name)//getrandom method accepts a string input
+        {
+           
+                return name +" "+rdm.Next(6, 15);/* appends the random 
+                                                    number generated between 6,15 
+                                                    to the input and returns as a result*/
+           
+        }      
+       
+    }
+}
